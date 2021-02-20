a3721a499b5c1c07526855f9ad4c3d4379c8fa8e
Akhil Bhalerao <55273506+iamakkkhil@users.noreply.github.com>
2021-02-20 11:26:54 +0530

    Day 20 Added

---
+++ Day_20.py
@@ -0,0 +1,34 @@
+"""
+DAY 20: Rotate by 90 degree.
+https://www.geeksforgeeks.org/rotate-a-matrix-by-90-degree-in-clockwise-direction-without-using-any-extra-space/
+
+QUESTION : Given a square matrix of size N x N. The task is to rotate it by 90 degrees in anti-clockwise 
+direction without using any extra space.
+
+Expected Time Complexity : O(N^2)
+Expected Auxilliary Space : O(1)
+
+Constraints:
+1 ≤ N ≤ 100
+1 <= matrix[][] <= 1000
+
+"""
+
+def rotate_90_matrix(mat, N):
+    
+    for i in range(int(N/2)):
+        for j in range(i, N-i-1):
+            
+            temp = mat[i][j]
+            mat[i][j] = mat[j][N-i-1]
+            mat[j][N-i-1] = mat[N-i-1][N-j-1]
+            mat[N-i-1][N-j-1] = mat[N-j-1][i]
+            mat[N-j-1][i] = temp
+
+
+
+matrix = [ [ 1, 2, 3, 4 ], [ 5, 6, 7, 8 ], [ 9, 10, 11, 12 ], [ 13, 14, 15, 16 ] ]
+
+rotate_90_matrix(matrix,len(matrix))
+print(matrix)
+
